9d82050d3e894f4c98cd77706e3117a12329a31b
soaadhallak <soaadhallak2@gmail.com>
2025-04-19 19:06:26 +0300

    initialize spatie media library and prepare models

--- app/Models/Place.php
+++ app/Models/Place.php
@@ -4,11 +4,13 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Spatie\MediaLibrary\HasMedia;
+use Spatie\MediaLibrary\InteractsWithMedia;
 
-class Place extends Model
+class Place extends Model implements HasMedia
 {
     /** @use HasFactory<\Database\Factories\PlaceFactory> */
-    use HasFactory;
+    use HasFactory,InteractsWithMedia;
     public function trips(){
         return $this->belongsToMany(Trip::class,'trip_place')->withPivot('min_price','max_price')->withTimestamps();
     }
--- app/Models/Story.php
+++ app/Models/Story.php
@@ -4,10 +4,12 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Spatie\MediaLibrary\HasMedia;
+use Spatie\MediaLibrary\InteractsWithMedia;
 
-class Story extends Model
+class Story extends Model implements HasMedia
 {
-    use HasFactory;
+    use HasFactory,InteractsWithMedia;
 
     public function user(){
         return $this->belongsTo(User::class);
